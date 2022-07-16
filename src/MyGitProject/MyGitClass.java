88a76480fb05f07226abddcd093ee8ea00853380
Esra Erbaş <erbase@gmail.com>
2022-07-16 22:47:46 +0300

    third commit

--- src/MyGitProject/MyGitClass.java
+++ src/MyGitProject/MyGitClass.java
@@ -4,5 +4,7 @@ public class MyGitClass {
     public static void main(String[] args) {
 
         System.out.println("ı m learning github!");
+        System.out.println("ı m adding new code...");
+        System.out.println("ı m adding one more");
     }
 }
